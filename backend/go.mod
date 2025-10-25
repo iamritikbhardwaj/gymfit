7d803265d96e1beeaa7d7ee8ac89e74cf5152834
Ritik Singh <ritiklrt2@gmail.com>
2025-10-25 13:47:35 +0530

    added frontend and backend

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/iamritikbhardwaj/gymfit
+
+go 1.25.1
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>GymFit Frontend</h1>
+    <p>Welcome to the GymFit Frontend</p>
+    </>
+  )
+}
+
+export default App
